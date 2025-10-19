17a509fbe83ac9105b8820c96d28b58f61d874ba
masonfrykman <masonfrykman@proton.me>
2025-10-19 14:15:34 -0500

    Update wording, try to prevent errors from crashing the entire server.

--- bin/portfolio.dart
+++ bin/portfolio.dart
@@ -5,56 +5,61 @@ import 'dart:typed_data';
 import 'package:rbws/rbws.dart';
 
 void main(List<String> args) {
-  // Load index
-  var index = File("public/index.html");
-  if (!index.existsSync()) {
-    stderr.writeln("Failed to find public/index.html, does not exist.");
-    exit(256);
-  }
+  try {
+    // Load index
+    var index = File("public/index.html");
+    if (!index.existsSync()) {
+      stderr.writeln("Failed to find public/index.html, does not exist.");
+      exit(256);
+    }
 
-  final mainPageContent = index.readAsStringSync();
+    final mainPageContent = index.readAsStringSync();
 
-  // Bring up insecure instance.
-  final insecureInstance = HTTPServerInstance(
-    InternetAddress.anyIPv4,
-    80,
-    generalServeRoot: "public/",
-  );
-
-  insecureInstance.routeNotFound = (r) {
-    return RBWSResponse.dataFromString(
-      200,
-      mainPageContent,
-      toRequest: r,
-      headers: {"Content-Type": "text/html"},
-    );
-  };
-
-  insecureInstance.start();
-  print(
-    "Brought up insecure instance at ${insecureInstance.host} @ port ${insecureInstance.port}",
-  );
-
-  // Try to bring up secure instance
-  if (Platform.environment["CERT_PRIV_KEY"] != null &&
-      Platform.environment["CERT_PATH"] != null) {
-    final securityContext = SecurityContext(withTrustedRoots: true);
-    securityContext.useCertificateChain(Platform.environment["CERT_PATH"]!);
-    securityContext.usePrivateKey(Platform.environment["CERT_PRIV_KEY"]!);
-
-    final secureInstance = HTTPServerInstance(
+    // Bring up insecure instance.
+    final insecureInstance = HTTPServerInstance(
       InternetAddress.anyIPv4,
-      443,
+      80,
       generalServeRoot: "public/",
-      securityContext: securityContext,
     );
 
-    secureInstance.routeNotFound = insecureInstance.routeNotFound;
+    insecureInstance.routeNotFound = (r) {
+      return RBWSResponse.dataFromString(
+        200,
+        mainPageContent,
+        toRequest: r,
+        headers: {"Content-Type": "text/html"},
+      );
+    };
 
-    secureInstance.start();
-
+    insecureInstance.start();
     print(
-      "Brought up secure instance at ${secureInstance.host} @ port ${secureInstance.port}",
+      "Brought up insecure instance at ${insecureInstance.host} @ port ${insecureInstance.port}",
     );
+
+    // Try to bring up secure instance
+    if (Platform.environment["CERT_PRIV_KEY"] != null &&
+        Platform.environment["CERT_PATH"] != null) {
+      final securityContext = SecurityContext(withTrustedRoots: true);
+      securityContext.useCertificateChain(Platform.environment["CERT_PATH"]!);
+      securityContext.usePrivateKey(Platform.environment["CERT_PRIV_KEY"]!);
+
+      final secureInstance = HTTPServerInstance(
+        InternetAddress.anyIPv4,
+        443,
+        generalServeRoot: "public/",
+        securityContext: securityContext,
+      );
+
+      secureInstance.routeNotFound = insecureInstance.routeNotFound;
+
+      secureInstance.start();
+
+      print(
+        "Brought up secure instance at ${secureInstance.host} @ port ${secureInstance.port}",
+      );
+    }
+  } catch (x) {
+    stderr.writeln("Caught exception:");
+    stderr.writeln(x);
   }
 }
