b301af5edce423392bc14504b1223a336d91831c
masonfrykman <masonfrykman@proton.me>
2025-12-03 08:54:32 -0600

    Minify css & html, switch index to use bundle.min.js

--- build.sh
+++ build.sh
@@ -7,16 +7,23 @@ rm .intermediate.js
 
 # Copy over assets
 
+cd public
 # HTML
-for filename in public/*.html; do
-    cp $filename dist/
+for filename in *.html; do
+    minify $filename > ../dist/$filename
 done
 
 # CSS
-for filename in public/*.css; do
-    cp $filename dist/
+for filename in *.css; do
+    minify $filename > ../dist/$filename
 done
+cd ..
 
+# individual files
+cp public/projects_info.json dist/projects_info.json
+cp public/sitemap.xml dist/sitemap.xml
+
+# Package for quick upload
 tar -cz dist/ > build.tar.gz
 
 echo "Done"
