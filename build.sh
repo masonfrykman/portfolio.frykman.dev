df42e0b3465848ce591d9cb1b791171dd373b85e
masonfrykman <masonfrykman@proton.me>
2025-12-04 15:08:41 -0600

    Rename dir public to src

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Minify JS
-rollup public/index.js --file .intermediate.js --format es
+rollup src/index.js --file .intermediate.js --format es
 minify .intermediate.js > dist/bundle.min.js
 rm .intermediate.js
 
 # Copy over assets
 
-cd public
+cd src
 # HTML
 for filename in *.html; do
     minify $filename > ../dist/$filename
@@ -20,8 +20,8 @@ done
 cd ..
 
 # individual files
-cp public/projects_info.json dist/projects_info.json
-cp public/sitemap.xml dist/sitemap.xml
+cp src/projects_info.json dist/projects_info.json
+cp src/sitemap.xml dist/sitemap.xml
 
 # Package for quick upload
 tar -cz dist/ > build.tar.gz
