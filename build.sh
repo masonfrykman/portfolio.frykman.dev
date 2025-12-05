ae1d20090a2c44322aaf9ef7f642e5c5d5422565
masonfrykman <masonfrykman@proton.me>
2025-12-04 22:11:29 -0600

    stop executing if build errors

--- build.sh
+++ build.sh
@@ -9,16 +9,28 @@ echo ""
 echo "Compiling Typescript to Javascript"
 echo "  > tsc"
 tsc
+if [[ $? -ne 0 ]]; then
+    echo "BUILD FAILED"
+    exit 1
+fi
 
 echo ""
 echo "Bundling & minifying Javascript"
 # Bundle & minify JS
 echo "  > rollup"
 rollup build/index.js --file build/bundle.js --format es
+if [[ $? -ne 0 ]]; then
+    echo "BUILD FAILED"
+    exit 1
+fi
 
 echo "  > minify"
 mkdir dist
 minify build/bundle.js > dist/bundle.min.js
+if [[ $? -ne 0 ]]; then
+    echo "BUILD FAILED"
+    exit 1
+fi
 
 # Copy over assets from src/ -> dist/
 echo ""
@@ -27,16 +39,33 @@ cd src
 # HTML
 for filename in *.html; do
     minify $filename > ../dist/$filename
+    if [[ $? -ne 0 ]]; then
+        echo "BUILD FAILED"
+        exit 1
+    fi
 done
 
 # CSS
 for filename in *.css; do
     minify $filename > ../dist/$filename
+    if [[ $? -ne 0 ]]; then
+        echo "BUILD FAILED"
+        exit 1
+    fi
 done
 cd ..
 
 cp src/projects_info.json dist/projects_info.json
+if [[ $? -ne 0 ]]; then
+    echo "BUILD FAILED"
+    exit 1
+fi
+
 cp src/sitemap.xml dist/sitemap.xml
+if [[ $? -ne 0 ]]; then
+    echo "BUILD FAILED"
+    exit 1
+fi
 
 echo ""
 echo "Bundling js + assets"
