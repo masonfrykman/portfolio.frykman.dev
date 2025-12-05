1d3fc59c7f4fad065a9a97839c1838ad1b451db4
masonfrykman <masonfrykman@proton.me>
2025-12-04 19:32:41 -0600

    Better fatal error screen

--- src/error.ts
+++ src/error.ts
@@ -1,10 +1,12 @@
 export function showError(errDesc: string) {
     console.error(errDesc)
 
-    document.body.innerHTML = `<h1>FATAL ERROR</h1><br>
-    <span id='errdesc'></span>`;
+    document.body.innerHTML = `<h1 style='font-size: 72pt !important;'>FATAL ERROR</h1>
+    <p id='errdesc'></p>`;
+
+    document.body.style = "background-color: #7b0000ff; color: yellow !important; flex-direction: column;"
 
     if(document.getElementById("errdesc") != null) {
-        document.getElementById("errdesc")!.innerText = errDesc;
+        document.getElementById("errdesc")!.innerHTML = errDesc;
     }
 }
