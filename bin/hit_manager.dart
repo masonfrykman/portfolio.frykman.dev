cf59aca887abaa57e986bcd69280df0e1096e9a6
masonfrykman <masonfrykman@proton.me>
2026-03-04 08:43:56 -0600

    Extract user agent

--- bin/hit_manager.dart
+++ bin/hit_manager.dart
@@ -2,38 +2,46 @@ import 'package:uuid/uuid.dart';
 
 class HitManager {
   Map<String, Map<String, int>> hits = {};
+  Map<String, Map<String, String>> attributes = {};
+
+  static Map<String, String> _getCookies(String header) {
+    final cookies = header.split("; ");
+    final map = <String, String>{};
+    for (var cookie in cookies) {
+      final spl = cookie.split("=");
+      if (spl.length < 2) continue;
+
+      map[spl.first] = spl[1];
+    }
+    return map;
+  }
 
   String? hit(String? cookieHeader, {String path = "unspecified"}) {
-    print(hits);
-
-    String? id;
-
-    // Get the ID from the cookie header if exists
-    if (cookieHeader != null) {
-      final cookies = cookieHeader.split("; ");
-      for (var cookie in cookies) {
-        if (cookie.startsWith("sid")) {
-          final pair = cookie.split("=");
-          if (pair.length != 2) {
-            continue;
-          }
-
-          id = pair[1];
-          break;
-        }
-      }
-    }
+    String? id = _getCookies(cookieHeader ?? "")["sid"];
 
     // If they don't have a valid id, make a new one and return it
     if (id == null || !hits.containsKey(id)) {
       var newID = Uuid().v4();
       hits[newID] = {};
       hits[newID]![path] = 1;
+      attributes[newID] = {};
       return newID;
     }
 
     // Otherwise, log their hit and return null.
     hits[id]![path] = (hits[id]![path] ?? 0) + 1;
     return null;
+  }
+
+  void userAgent(String? cookieHeader, {String? id}) {
+    final cookies = _getCookies(cookieHeader ?? "");
+
+    String? sid = _getCookies(cookieHeader ?? "")["sid"] ?? id;
+    if (sid == null) return;
+
+    if (!cookies.containsKey("User-Agent")) return;
+
+    attributes[sid] ??= {};
+    attributes[sid]!["User-Agent"] = cookies["User-Agent"]!;
   }
 }
